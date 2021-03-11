1cfa809549b45d7c706fb363e7504effb1d3fc62
Mahh <the_mahh@mail.ru>
2021-03-11 20:56:58 +0300

    added gini and shannon realisation

---
+++ task_4/gini_tree.py
@@ -0,0 +1,139 @@
+import numpy as np
+class Node:
+
+    def __init__(self, index, t, true_branch, false_branch):
+        self.index = index  # индекс признака, по которому ведется сравнение с порогом в этом узле
+        self.t = t  # значение порога
+        self.true_branch = true_branch  # поддерево, удовлетворяющее условию в узле
+        self.false_branch = false_branch  # поддерево, не удовлетворяющее условию в узле
+
+
+# И класс терминального узла (листа)
+
+class Leaf:
+
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+        self.prediction = self.predict()
+
+    def predict(self):
+        # подсчет количества объектов разных классов
+        classes = {}  # сформируем словарь "класс: количество объектов"
+        for label in self.labels:
+            if label not in classes:
+                classes[label] = 0
+            classes[label] += 1
+        #  найдем класс, количество объектов которого будет максимальным в этом листе и вернем его
+        prediction = max(classes, key=classes.get)
+        return prediction
+
+
+def gini(labels):
+    #  подсчет количества объектов разных классов
+    classes = {}
+    for label in labels:
+        if label not in classes:
+            classes[label] = 0
+        classes[label] += 1
+
+    #  расчет критерия
+    impurity = 1
+    for label in classes:
+        p = classes[label] / len(labels)
+        impurity -= p ** 2
+
+    return impurity
+
+
+def quality(left_labels, right_labels, current_gini):
+    # доля выбоки, ушедшая в левое поддерево
+    p = float(left_labels.shape[0]) / (left_labels.shape[0] + right_labels.shape[0])
+
+    return current_gini - p * gini(left_labels) - (1 - p) * gini(right_labels)
+
+
+# Разбиение датасета в узле
+
+def split(data, labels, index, t):
+    left = np.where(data[:, index] <= t)
+    right = np.where(data[:, index] > t)
+
+    true_data = data[left]
+    false_data = data[right]
+    true_labels = labels[left]
+    false_labels = labels[right]
+
+    return true_data, false_data, true_labels, false_labels
+
+
+# Нахождение наилучшего разбиения
+
+def find_best_split(data, labels):
+    #  обозначим минимальное количество объектов в узле
+    min_leaf = 5
+
+    current_gini = gini(labels)
+
+    best_quality = 0
+    best_t = None
+    best_index = None
+
+    n_features = data.shape[1]
+
+    for index in range(n_features):
+        # будем проверять только уникальные значения признака, исключая повторения
+        t_values = np.unique([row[index] for row in data])
+
+        for t in t_values:
+            true_data, false_data, true_labels, false_labels = split(data, labels, index, t)
+            #  пропускаем разбиения, в которых в узле остается менее 5 объектов
+            if len(true_data) < min_leaf or len(false_data) < min_leaf:
+                continue
+
+            current_quality = quality(true_labels, false_labels, current_gini)
+
+            #  выбираем порог, на котором получается максимальный прирост качества
+            if current_quality > best_quality:
+                best_quality, best_t, best_index = current_quality, t, index
+
+    return best_quality, best_t, best_index
+
+
+# Построение дерева с помощью рекурсивной функции
+
+def build_tree(data, labels):
+    quality, t, index = find_best_split(data, labels)
+
+    #  Базовый случай - прекращаем рекурсию, когда нет прироста в качества
+    if quality == 0:
+        return Leaf(data, labels)
+
+    true_data, false_data, true_labels, false_labels = split(data, labels, index, t)
+
+    # Рекурсивно строим два поддерева
+    true_branch = build_tree(true_data, true_labels)
+    false_branch = build_tree(false_data, false_labels)
+
+    # Возвращаем класс узла со всеми поддеревьями, то есть целого дерева
+    return Node(index, t, true_branch, false_branch)
+
+
+def classify_object(obj, node):
+    #  Останавливаем рекурсию, если достигли листа
+    if isinstance(node, Leaf):
+        answer = node.prediction
+        return answer
+
+    if obj[node.index] <= node.t:
+        return classify_object(obj, node.true_branch)
+    else:
+        return classify_object(obj, node.false_branch)
+
+
+def predict(data, tree):
+    classes = []
+    for obj in data:
+        prediction = classify_object(obj, tree)
+        classes.append(prediction)
+    return classes
